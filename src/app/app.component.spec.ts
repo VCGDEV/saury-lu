5b8388c26f58a2f3f844ccd33f399b5f3f99bd90
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-09-29 20:30:52 -0500

    update main component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,61 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
 
-describe('Main app', () =>{
-  it('should be true',() => {
-    expect(5+5).toEqual(10);
-  })
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslateService} from "@ngx-translate/core";
+import { MyApp } from './app.component';
+import {Observable} from "rxjs";
+
+describe('MyApp', () => {
+  let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy,
+  translateSpy,translateLngSpy, getSpy;
+
+  beforeEach(async(() => {
+    statusBarSpy = { styleDefault: jest.fn() };
+    splashScreenSpy = { hide: jest.fn() };
+    platformReadySpy = jest.fn().mockImplementation(() => Promise.resolve());
+    platformSpy = {
+      ready: platformReadySpy
+    };
+
+    translateLngSpy = jest.fn().mockImplementation((str) => str);
+    getSpy = jest.fn().mockImplementation((str) => Observable.create(observer => observer.next(str)));
+
+    translateSpy = {
+      setDefaultLang: translateLngSpy,
+      get: getSpy
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [MyApp],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: StatusBar, useValue: statusBarSpy },
+        { provide: SplashScreen, useValue: splashScreenSpy },
+        { provide: Platform, useValue: platformSpy },
+        { provide: TranslateService, useValue: translateSpy}
+      ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(MyApp);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should initialize the app', async () => {
+    TestBed.createComponent(MyApp);
+    expect(platformSpy.ready).toHaveBeenCalled();
+    await platformReadySpy();
+    expect(statusBarSpy.styleDefault).toHaveBeenCalled();
+    expect(splashScreenSpy.hide).toHaveBeenCalled();
+  });
+
+  it('should translate 10 items', () => {
+    TestBed.createComponent(MyApp);
+    expect(translateSpy.get).toBeCalledTimes(10);
+  });
 });
