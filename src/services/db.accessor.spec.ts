572ca25093b34f866dfd38472a9cf74b3606608a
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-10-01 23:47:23 -0500

    create find all query from entity

--- src/services/db.accessor.spec.ts
+++ src/services/db.accessor.spec.ts
@@ -7,13 +7,19 @@ describe('DatabaseAccessor', () => {
   it('should create get by id query', () =>{
     const expectedSQL = `select systemId, name from ${TABLE_N.System} where systemId=?`;
     const query = dbAccesss._createGetByIdQuery(new System());
-    expect(expectedSQL).toEqual(query);
+    expect(query).toEqual(expectedSQL);
   });
 
   it('should create insert query', () =>{
     const expectedSQL = `insert into ${TABLE_N.System}(systemId, name) values(?, ?)`;
     const query = dbAccesss._createInsertQuery(new System());
-    expect(expectedSQL).toEqual(query);
+    expect(query).toEqual(expectedSQL);
+  });
+
+  it('should create find all query', () =>{
+    const expectedSQL = `select systemId, name from ${TABLE_N.System}`;
+    const query = dbAccesss._createFindAllQuery(new System());
+    expect(query).toEqual(expectedSQL);
   });
 
 });
