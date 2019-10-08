67ba91e7a26e5f2ab4417416b0ed8cbcd58e3b4b
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-10-07 22:37:23 -0500

    add page to update category

--- src/pages/categories/categories.module.ts
+++ src/pages/categories/categories.module.ts
@@ -3,11 +3,13 @@ import { IonicPageModule } from 'ionic-angular';
 import { CategoriesPage } from './categories';
 import {AddCategoryComponent} from "./add/add.category.component";
 import {TranslateModule} from "@ngx-translate/core";
+import {UpdateCategoryComponent} from "./update/update.category.component";
 
 @NgModule({
   declarations: [
     CategoriesPage,
     AddCategoryComponent,
+    UpdateCategoryComponent
   ],
   imports: [
     IonicPageModule.forChild(CategoriesPage),
@@ -15,7 +17,8 @@ import {TranslateModule} from "@ngx-translate/core";
   ],
   entryComponents: [
     CategoriesPage,
-    AddCategoryComponent
+    AddCategoryComponent,
+    UpdateCategoryComponent
   ]
 })
 export class CategoriesPageModule {}
